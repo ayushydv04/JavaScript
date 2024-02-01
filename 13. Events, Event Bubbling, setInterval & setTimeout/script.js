6ea342d1e8379059431b5872c09cf50fff8f0e80
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-01 23:56:55 +0530

    commit 1/2/24

---
+++ 13. Events, Event Bubbling, setInterval & setTimeout/script.js
@@ -0,0 +1,38 @@
+// this is how we do
+// making a variable then doing it then adding event listener
+let button = document.getElementById("btn")
+
+// button.addEventListener("click", ()=>{
+//     // alert("I was clicked")
+//     document.querySelector(".box").innerHTML = "<b>I am clicked</b>"
+// })
+
+// for event reference
+// List of all mouse evenets
+// https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
+
+
+// double click
+// button.addEventListener("dblclick", ()=>{
+//     // alert("I was clicked")
+//     document.querySelector(".box").innerHTML = "<b>I am clicked</b>"
+// })
+
+// mousedown -->  means when mouse is clicked
+
+// mouseleave --> when we take arrow and we remove
+
+
+// contextmenu --> to open when right click
+// button.addEventListener("contextmenu", ()=>{
+//     alert("Right click is prohibited")
+// })
+
+// key down
+// document.addEventListener("keydown", (e)=>{
+//     console.log(e.key);
+// })
+
+
+// Form events
+
